18dfa788dc2c818dbb574c394225ae1a303af906
matuszen <mateuszn641@gmail.com>
2024-05-18 07:35:54 +0200

    Optimize & modify runge kutta algorithm (4th order)
    - add plot to visualize how function works
    - add new docstring
    - arguments was typed
    - add execution time

--- maths/numerical_analysis/runge_kutta.py
+++ maths/numerical_analysis/runge_kutta.py
@@ -1,38 +1,84 @@
+import math
+from collections.abc import Callable, Iterable
+
 import numpy as np
 
 
-def runge_kutta(f, y0, x0, h, x_end):
+def runge_kutta_method(
+    x0: float,
+    y0: float,
+    h: float,
+    x_end: float,
+    fun: Callable[[float, float], float],
+) -> Iterable[float]:
     """
-    Calculate the numeric solution at each step to the ODE f(x, y) using RK4
+    Solves an ordinary differential equation using the 4th-order Runge-Kutta method.
 
-    https://en.wikipedia.org/wiki/Runge-Kutta_methods
+    Parameters
+    ----------
+    x0 : float
+        The initial value of x.
+    y0 : float
+        The initial value of y.
+    h : float
+        The step size for the numerical solution.
+    x_end : float
+        The value of x at which to stop the computation.
+    fun : (float, float) -> float
+        A function that returns the derivative of y at given values of x and y.
 
-    Arguments:
-    f -- The ode as a function of x and y
-    y0 -- the initial value for y
-    x0 -- the initial value for x
-    h -- the stepsize
-    x_end -- the end value for x
+    Returns
+    -------
+    Iterable[float]
+        Array of y values at each step from x0 to x_end.
 
-    >>> # the exact solution is math.exp(x)
-    >>> def f(x, y):
-    ...     return y
+    Raises
+    ------
+    ValueError
+        If `x0` is greater than or equal to `x_end`.
+        If `h` is not positive.
+
+    Examples
+    --------
+    >>> def fun(x, y):
+    ...     return x + y
+    >>> x0 = 0
     >>> y0 = 1
-    >>> y = runge_kutta(f, y0, 0.0, 0.01, 5)
-    >>> y[-1]
-    148.41315904125113
+    >>> h = 0.1
+    >>> x_end = 0.5
+    >>> runge_kutta_method(x0, y0, h, x_end, fun)
+    array([1.        , 1.11034167, 1.24280596, 1.39846959, 1.57852783, 1.78429709])
+
+    Notes
+    -----
+    The 4th-order Runge-Kutta method is an iterative technique that provides a
+    numerical solution to ordinary differential equations of the form dy/dx = f(x, y).
+    The method is based on taking the average of four increments to estimate the
+    value of y at the next step.
     """
-    n = int(np.ceil((x_end - x0) / h))
-    y = np.zeros((n + 1,))
-    y[0] = y0
+
+    if x0 >= x_end:
+        raise ValueError(
+            "The final value of x must be greater than the first value of x"
+        )
+
+    if h <= 0:
+        raise ValueError("Step size must be positive")
+
+    n = int(math.ceil((x_end - x0) / h))
+    y = np.zeros(n + 1)
+
     x = x0
+    y[0] = y0
+    sixth_h = h / 6
 
     for k in range(n):
-        k1 = f(x, y[k])
-        k2 = f(x + 0.5 * h, y[k] + 0.5 * h * k1)
-        k3 = f(x + 0.5 * h, y[k] + 0.5 * h * k2)
-        k4 = f(x + h, y[k] + h * k3)
-        y[k + 1] = y[k] + (1 / 6) * h * (k1 + 2 * k2 + 2 * k3 + k4)
+        k1 = fun(x, y[k])
+        h_k1_2 = 0.5 * h * k1
+        k2 = fun(x + 0.5 * h, y[k] + h_k1_2)
+        k3 = fun(x + 0.5 * h, y[k] + 0.5 * h * k2)
+        k4 = fun(x + h, y[k] + h * k3)
+        y[k + 1] = y[k] + sixth_h * (k1 + 2 * k2 + 2 * k3 + k4)
         x += h
 
     return y
@@ -40,5 +86,45 @@ def runge_kutta(f, y0, x0, h, x_end):
 
 if __name__ == "__main__":
     import doctest
+    import timeit
+
+    import matplotlib.pyplot as plt
 
     doctest.testmod()
+
+    def fun(x: float, y: float) -> float:
+        return x + 0.6 * y
+
+    X0 = 0.0
+    Y0 = 0.2
+    # If H is smaller, the chart will be more accurate,
+    # compare this chart with chart with H = 0.2
+    H = 0.5
+    X_END = 5
+
+    print(
+        "Execution time:",
+        timeit.timeit(
+            "runge_kutta_method(X0, Y0, H, X_END, fun)",
+            number=1000,
+            globals=globals(),
+        ),
+    )
+
+    X = [round(X0 + H * i, 2) for i in range(int((X_END - X0) / H) + 1)]
+    Y = runge_kutta_method(X0, Y0, H, X_END, fun)
+
+    with plt.style.context("bmh"):
+        fig, ax = plt.subplots()
+
+        ax.plot(X, Y, "r", label="f(x, y) = x + 0.6 * y")
+
+        ax.set_title("Runge-Kutta method")
+        ax.set_xlabel("X Axis")
+        ax.set_ylabel("Y Axis")
+
+        fig.set_facecolor("#ddd")
+        ax.set_facecolor("#ddd")
+
+        plt.legend()
+        plt.show()
